2319d4802504eec57cc44803776cd46e0f2d1bcf
Matthew Nguyen <76631936+mattwhen@users.noreply.github.com>
2023-01-25 00:05:43 -0600

    added parameters and appended data in weather status container

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+
 // How do I hide the API key?
 let apiKey = 'fffae12063e8e68b76c3c77004656139';
 
@@ -8,28 +9,46 @@ function callGeoApi() {
     let fetchUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + userInput + '&limit=1&appid=' + apiKey;
 
     fetch(fetchUrl)
+
     .then(function(response) {
         return response.json();
     })
+
     .then(function(data) {
         console.log(data);
         let lat = data[0].lat;
         let lon = data[0].lon;
-        call5DayData(lat, lon);
+        call5DayData(lat, lon); // Call the 5 day weather forecast API with the variables that have the stored latitude and longitude as an argument. 
     })
 }
 
-function call5DayData(lat, lon) {
+function call5DayData(lat, lon) { // Pass the values from the previous fetch request into the function parameters
 
-    let fetchUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+    let fetchUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`;
 
     fetch(fetchUrl)
     .then(function(response) {
         return response.json();
     })
     .then(function(data) {
+
         console.log(data);
+        
+       let centerContent = document.querySelector('.center-content');
+       let createTemperatureDiv = document.createElement('div');
+       createTemperatureDiv.textContent = 'Temp: ' + data.list[0].main.temp;
+       centerContent.appendChild(createTemperatureDiv);
+
+       let createWindDiv = document.createElement('div');
+       createWindDiv.textContent = 'Wind: ' + data.list[0].wind.speed;
+       centerContent.appendChild(createWindDiv);
+
+       let createHumidDiv = document.createElement('div');
+       createHumidDiv.textContent = 'Humidity: ' + data.list[0].wind.gust;
+       centerContent.appendChild(createHumidDiv);
     })
 }
 
+
 document.getElementById('submit-btn').onclick = callGeoApi;
+
